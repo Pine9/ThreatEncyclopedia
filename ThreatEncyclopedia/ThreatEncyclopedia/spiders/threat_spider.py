2cdaf668a7a41688a7834e2db5697ea993b544b7
Emma Adelmann <emmabadelmann@gmail.com>
2021-04-17 23:28:46 -0400

    things are running better now, but positions of some fields are inconsistent in the HTML so I need to find a way to search for them

--- ThreatEncyclopedia/ThreatEncyclopedia/spiders/threat_spider.py
+++ ThreatEncyclopedia/ThreatEncyclopedia/spiders/threat_spider.py
@@ -3,6 +3,8 @@ import scrapy
 
 class ThreatSpider(scrapy.Spider):
     name = "threat"
+    pages = 50
+    pagenum = 1
 
     start_urls = [
         'https://www.trendmicro.com/vinfo/us/threat-encyclopedia/malware'
@@ -14,24 +16,56 @@ class ThreatSpider(scrapy.Spider):
         for entry in response_list:
             url = response.urljoin(entry)
             yield scrapy.Request(url=url, callback=self.parse_entry)
-        next_page = response.css("li.pagesnumber a::attr('href')").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
+        # The html for navigating through the pages is a bit of a mess.
+        # However, I know that there are 50 pages
+        if self.pagenum <= self.pages:
+            self.pagenum += 1
+            next_page = response.urljoin(f"/vinfo/us/threat-encyclopedia/malware/page/{self.pagenum}")
             yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_entry(self, response):
         # fields: name, type, prefix, platform, suffix/variant, payload, infection channel, memory resident, file type, file size
         # malware naming conventions: https://docs.trendmicro.com/all/ent/tms/v2.5/en-us/tda_2.5_olh/malware_naming.htm
-        title = response.css("h1.lessen_h1::text").get().split('.')
+        title = response.css("h1.lessen_h1::text").get()
+        platform = response.css("div.entityHeader p::text").getall()
+        payload = response.css("div.labelHeader::text").getall()
+        resident = response.css("div.labelHeader::text").getall()
+        fileinfo = response.css("div.labelHeader::text").getall()[2]
+
+        if (len(platform)) > 1:
+            platform = platform[1]
+        else:
+            platform = "Unknown"
+
+        if len(payload) > 4:
+            payload = payload[4]
+        else:
+            payload = "Unknown"
+
+        if len(resident) > 3:
+            resident = resident[3]
+        else:
+            resident = None
+
+        if len(fileinfo) > 2:
+            filetype = fileinfo[2]
+        else:
+            filetype = "Unknown"
+
+        if len(fileinfo) > 1:
+            filesize = fileinfo[1]
+        else:
+            filesize = 0
+
+        """
+            'file type' : filetype,
+            'file size' : filesize
+        """
+        # look into prefix and suffix, which unfortunately is not consistent between samples
         yield {
-            'name' : title[2],
-            'type' : title[0],
-            'prefix' : title[1],
-            'suffix' : title[3],
-            'platform': response.css("div.entityHeader p::text").getall()[1],
-            'payload' : response.css("div.labelHeader::text").getall()[4],
-            'infection channel' : response.css("div.labelHeader::text").getall()[0],
-            'memory resident?' : response.css("div.labelHeader::text").getall()[3],
-            'file type' : response.css("div.labelHeader::text").getall()[2],
-            'file size' : response.css("div.labelHeader::text").getall()[1]
+            'full name' : title,
+            'platform': platform,
+            'payload' : payload,
+            'infection channel' : response.css("div.labelHeader::text").get(),
+            'memory resident?' : resident
         }
